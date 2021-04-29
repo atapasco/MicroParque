454531336026fed58d563376267d50b7497421fc
pipetapasco21 <pipetapasco21@outlook.com>
2021-04-29 15:52:35 -0600

    Agregar archivos de proyecto.

---
+++ Entity/Manipulador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Manipulador
+    {
+        public string Identificacion { set; get; }
+        public string PrimerNombre { set; get; }
+        public string SegundoNombre { set; get; }
+        public string PrimerApellido { set; get; }
+        public string SegundoApellido { set; get; }
+        public string Telefono { set; get; }
+    }
+}
